74eb1f950bf4f90b1582b4b4204559d28374e9df
MeIsOptical <tobixepremium@gmail.com>
2026-09-15 20:23:34 -0400

    added properties to fetchurl for more possibilities

--- tools/scripting/default_scripts/Fetch URL/code.js
+++ tools/scripting/default_scripts/Fetch URL/code.js
@@ -5,8 +5,25 @@ async function main(inputs) {
     }
 
     try {
+        // Construct fetch options
+        const options = {
+            method: inputs.method ? inputs.method.toUpperCase() : 'GET'
+        };
+
+        // Attach body if provided
+        if (inputs.body) {
+            options.body = typeof inputs.body === 'string' ? inputs.body : JSON.stringify(inputs.body);
+
+            // Automatically append JSON content-type if an object was passed
+            if (typeof inputs.body === 'object') {
+                options.headers = {
+                    'Content-Type': 'application/json'
+                };
+            }
+        }
+
         // Fetch the URL
-        const response = await fetch(inputs.url);
+        const response = await fetch(inputs.url, options);
         const text = await response.text();
 
         return {
